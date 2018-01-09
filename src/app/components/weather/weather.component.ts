767322b65e867cbc92ceed037c883e87945af593
StepanxXx <stepanxxx@gmail.com>
2018-01-10 00:14:24 +0200

    second commit

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -19,26 +19,6 @@ export class WeatherComponent implements OnInit {
     private _chart: ChartService
   ) {}
   ngOnInit(){
-/*
-    this._weather.dailyForecast("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").subscribe((res:any) => {
-        let data= res.query.results.channel.item.forecast;
-        let temp_max=data.map(res => res.high);
-        let temp_min=data.map(res => res.low);
-        let weatherDates=data.map(res => res.date);
-
-        this.chart0=this._chart.makeChart(
-          'canvas0',
-          'Weather',
-          [
-            {name:'min, Temperature', color:'#0039dd', data:temp_min, id:'y1'},
-            {name:'max, Temperature', color:'#dd0031', data:temp_max, id:'y1'},
-          ],
-          weatherDates,
-          'Date',
-          [{p:'left', id:'y1', name:'Temperature'}]
-        )
-      })
-*/
       this._weather.dailyForecast("https://kuna.io/api/v2/trades?market=btcuah").subscribe((res:any) => {
           let data:any = res.reverse();
           let price=data.map(res => res.price*1);
