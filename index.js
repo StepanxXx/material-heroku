998b792fe9f93b58f7909a890961693bf8365ba2
StepanxXx <stepanxxx@gmail.com>
2018-12-19 21:10:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+var compression = require('compression');
 const app = express();
+app.use(compression());
 const PORT = process.env.PORT || 5000
 app.use(express.static(__dirname + '/dist'));
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
